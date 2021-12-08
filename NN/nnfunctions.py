6ce97a042053d450e74421920385d763786e0e4e
KhelmholtzR <kevinrao2000@gmail.com>
2021-12-08 16:22:49 -0500

    Cleaned up nn notebook and reran training for 200 epochs.

--- NN/nnfunctions.py
+++ NN/nnfunctions.py
@@ -4,179 +4,16 @@ Neural network functions for scene segmentation. Based off of
 """
 from keras import layers
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from keras.callbacks import EarlyStopping
-from keras.optimizers import Adam
 from keras.layers import Conv2D, Conv2DTranspose, MaxPool2D, concatenate
-from proglearn.deciders import SimpleArgmaxAverage
-from proglearn.progressive_learner import ProgressiveLearner
-from proglearn.transformers import (
-    NeuralClassificationTransformer,
-)
 from keras import Model
-from proglearn.voters import KNNClassificationVoter
 
-from proglearn.network import LifelongClassificationNetwork
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
 
-from tensorflow.keras.backend import clear_session
-
-def run_fte_bte_exp(data_x, data_y, which_task, num_tasks, network, shift=0):
-
-    df_total = []
-
-    for slot in range(
-        1
-    ):  # Rotates the batch of training samples that are used from each class in each task
-        train_x, train_y, test_x, test_y = cross_val_data(data_x, data_y, shift, slot)
-
-        clear_session()  # clear GPU memory before each run, to avoid OOM error
-
-        default_transformer_class = NeuralClassificationTransformer
-
-        default_transformer_kwargs = {
-            "network": network,
-            "euclidean_layer_idx": -2,
-            "loss": "categorical_crossentropy",
-            "optimizer": Adam(3e-4),
-            "fit_kwargs": {
-                "epochs": 100,
-                "callbacks": [EarlyStopping(patience=5, monitor="val_loss")],
-                "verbose": False,
-                "validation_split": 0.33,
-                "batch_size": 32,
-            },
-        }
-        default_voter_class = KNNClassificationVoter
-        default_voter_kwargs = {"k": int(np.log2(360))}
-        default_decider_class = SimpleArgmaxAverage
-
-        p_learner = ProgressiveLearner(
-            default_transformer_class=default_transformer_class,
-            default_transformer_kwargs=default_transformer_kwargs,
-            default_voter_class=default_voter_class,
-            default_voter_kwargs=default_voter_kwargs,
-            default_decider_class=default_decider_class,
-        )
-
-        df = fte_bte_experiment(
-            train_x,
-            train_y,
-            test_x,
-            test_y,
-            shift,
-            slot,
-            num_tasks,
-            p_learner,
-            which_task,
-            acorn=12345,
-        )
-
-        df_total.append(df)
-
-    return df_total
-
-def fte_bte_experiment(
-    train_x,
-    train_y,
-    test_x,
-    test_y,
-    shift,
-    slot,
-    num_tasks,
-    p_learner,
-    which_task,
-    acorn=None,
-):
-
-    # We initialize lists to store the results
-    df = pd.DataFrame()
-    accuracies_across_tasks = []
-
-    # Declare the progressive learner model (Odif or Odin)
-    learner = p_learner
-
-    for task_num in range((which_task - 1), num_tasks):
-        # print("Starting Task {} For Shift {} For Slot {}".format(task_num, shift, slot))
-        if acorn is not None:
-            np.random.seed(acorn)
-
-        # If first task, add task.
-        if task_num == (which_task - 1):
-            learner.add_task(
-                X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
-                y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
-                task_id=task_num,
-                num_transformers=1,
-                transformer_voter_decider_split=[0.67, 0.33, 0],
-                decider_kwargs={
-                    "classes": np.unique(
-                        train_y[(task_num * 360) : ((task_num + 1) * 360)]
-                    )
-                },
-            )
-            t_num = 0
-            # Add tasks for all tasks up to current task (task t)
-            while t_num < task_num:
-                # Make a prediction on task t using the trained learner on test data
-                llf_task = learner.predict(
-                    test_x[((which_task - 1) * 120) : (which_task * 120), :],
-                    task_id=task_num,
-                )
-                acc = np.mean(
-                    llf_task == test_y[((which_task - 1) * 120) : (which_task * 120)]
-                )
-                accuracies_across_tasks.append(acc)
-
-                learner.add_task(
-                    X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
-                    y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
-                    task_id=t_num,
-                    num_transformers=1,
-                    transformer_voter_decider_split=[0.67, 0.33, 0],
-                    decider_kwargs={
-                        "classes": np.unique(
-                            train_y[(task_num * 360) : ((task_num + 1) * 360)]
-                        )
-                    },
-                )
-    
-                # Add task for next task
-                t_num = t_num + 1
-
-        else:
-            learner.add_task(
-                X=train_x[(task_num * 360) : ((task_num + 1) * 360)],
-                y=train_y[(task_num * 360) : ((task_num + 1) * 360)],
-                task_id=task_num,
-                num_transformers=1,
-                transformer_voter_decider_split=[0.67, 0.33, 0],
-                decider_kwargs={
-                    "classes": np.unique(
-                        train_y[(task_num * 360) : ((task_num + 1) * 360)]
-                    )
-                },
-            )
-
-        # Make a prediction on task t using the trained learner on test data
-        predictions = learner.predict(
-            test_x[((which_task - 1) * 120) : (which_task * 120), :],
-            task_id=(which_task - 1),
-        )
-        acc = np.mean(
-            predictions == test_y[((which_task - 1) * 120) : (which_task * 120)]
-        )
-        accuracies_across_tasks.append(acc)
-        # print("Accuracy Across Tasks: {}".format(accuracies_across_tasks))
-
-    df["task"] = range(1, num_tasks + 1)
-    df["task_accuracy"] = accuracies_across_tasks
-
-    return df
-
-def unet(input, num_classes, num_input_neurons=64):
-    conv1 = Conv2D(num_input_neurons, (3,3), activation='relu', padding='same')(input)
+def unet(input_layer, num_classes, num_input_neurons=64):
+    conv1 = Conv2D(num_input_neurons, (3,3), activation='relu', padding='same')(input_layer)
     conv1 = Conv2D(num_input_neurons, (3,3), activation='relu', padding='same')(conv1)
     max_pool1 = MaxPool2D((2,2))(conv1)
 
@@ -216,4 +53,111 @@ def unet(input, num_classes, num_input_neurons=64):
     up_conv4 = Conv2D(num_input_neurons, (3,3), activation='relu', padding='same')(up_conv4)
     up_conv4 = Conv2D(num_input_neurons, (3,3), activation='relu', padding='same')(up_conv4)
 
-    return Model(input = input, output = Conv2D(num_classes, (1,1), padding='same', activation='sigmoid')(up_conv4))
+    return Model(inputs = input_layer, outputs = Conv2D(num_classes, (1,1), padding='same', activation='softmax')(up_conv4))
+
+def load_images(imgpaths, lblpaths):
+    X = np.array([cv2.imread(imgpath) for imgpath in imgpaths], dtype = object)
+    Y = np.array([cv2.imread(lblpath) for lblpath in lblpaths], dtype = object)
+    return X,Y
+
+
+def load_image(image, mask):
+  input_image = tf.image.resize(image, (256, 256), method='nearest')
+  input_image = (input_image - tf.reduce_min(input_image)) / (tf.reduce_max(input_image) - tf.reduce_min(input_image)) * 255
+  input_mask = tf.image.resize(mask, (256, 256), method='nearest')
+  input_mask = (input_mask - tf.reduce_min(input_mask)) / (tf.reduce_max(input_mask) - tf.reduce_min(input_mask)) * 252
+  return input_image, input_mask
+
+def dice(y_true, y_pred):
+    intersection = tf.reduce_sum(tf.cast(tf.equal(y_true, y_pred), tf.float32))
+    return (intersection * 2.0) / tf.cast((tf.size(y_true) + tf.size(y_pred)), tf.float32)
+
+def load_data(imgpath, lblpath):
+    input_image, input_mask = load_images(imgpath, lblpath)
+    X = np.zeros((input_image.shape[0], 256, 256, 3))
+    Y = np.zeros((input_mask.shape[0], 256, 256, 3))
+    
+    for i, (x, y) in enumerate(zip(input_image, input_mask)):
+        X[i], Y[i] = tf.cast(load_image(x,y), tf.int16)
+
+    n_classes = len(np.unique(Y.reshape(-1, 3), axis = 0))
+    print("{} classes found".format(n_classes))
+    Y = relabel(Y, n_classes)
+    return X, Y, n_classes
+
+def relabel(Y_old, n_classes):
+    Y = np.zeros((Y_old.shape[0], 256, 256, 1))
+
+    # relabeling
+    for i, j in zip(np.unique(Y_old.reshape(-1, 3), axis = 0), np.arange(n_classes)):
+        where = (Y_old == i).all(axis=3)
+        Y[where] = j
+    
+    return Y
+
+def evaluate(model, X_test, Y_test):
+    dices = np.zeros(len(X_test))
+    accuracy = np.zeros(len(X_test))
+    mut_info = np.zeros(len(X_test))
+    for i in np.arange(len(X_test)):
+        m = tf.keras.metrics.Accuracy()
+        pred = model.predict(tf.convert_to_tensor([X_test[i]]))
+        score = dice(Y_test[i], pred)
+        plt.figure(figsize=(24, 5))
+        plt.subplot(1, 4, 1)
+        plt.imshow(tf.cast(X_test[i], tf.int32))
+        plt.title("Image")
+        plt.subplot(1,4,2)
+        plt.imshow(Y_test[i].reshape(256,256), cmap = "Set1")
+        plt.title("Ground Truth Segmentation Mask")
+        plt.subplot(1,4,3)
+        plt.imshow(np.argmax(pred, axis=3).reshape(256,256), cmap= "Set1")
+        plt.title("Predicted Mask (Dice: {:.2f})".format(score.numpy()))
+        dices[i] = score.numpy()
+        m.update_state(Y_test[i].flatten(), np.argmax(pred, axis=3).flatten())
+        accuracy[i] = m.result().numpy()
+        plt.subplot(1,4,4)
+        hist, x_edge, y_edge = np.histogram2d(Y_test[i].ravel(), np.argmax(pred,axis=3).ravel(), bins=20)
+        plt.imshow(hist.T, cmap='gray', origin="lower", extent=[x_edge[0], x_edge[-1], y_edge[0], y_edge[-1]])
+        plt.xlabel("Ground Truth Segmentation Mask Label Bins")
+        plt.ylabel("Predicted Mask Label Bins")
+        mut_info[i] = mutual_information(hist)
+        plt.title("Join Probability (Mutual Information: {:.2f}".format(mut_info[i]))
+    print("Mean Test Dice: {:.2f}".format(np.mean(dices)))
+    print("Mean Test Accuracy: {:.2f}".format(np.mean(accuracy)))
+
+def mutual_information(hist):
+    pxy = hist / float(np.sum(hist))
+    
+    # probability of x
+    px = np.sum(pxy, axis=1)
+    py = np.sum(pxy, axis=0)
+    px_py = px[:, None] * py[None, :]
+    return np.sum(pxy[pxy > 0] * np.log(pxy[pxy > 0] / px_py[pxy > 0]))
+
+def plot_metrics(history):
+    epochs = history.epoch
+    plt.figure(figsize=(22,5))
+
+    plt.subplot(1,3,1)
+    plt.plot(epochs, history.history['loss'], label="Training Loss")
+    plt.plot(epochs, history.history['val_loss'], label="Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Sparse Categorical Cross Entropy Loss")
+    plt.title("Loss vs. Epoch")
+    plt.legend()
+
+    plt.subplot(1,3,2)
+    plt.plot(epochs, history.history['accuracy'], label="Training Accuracy")
+    plt.plot(epochs, history.history['val_accuracy'], label="Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy vs. Epoch")
+    plt.legend()
+    plt.subplot(1,3,3)
+    plt.plot(epochs, history.history['dice'], label="Training Dice")
+    plt.plot(epochs, history.history['val_dice'], label="Validation Dice")
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Dice Coefficient")
+    plt.title("Dice Coefficient vs. Epoch")
